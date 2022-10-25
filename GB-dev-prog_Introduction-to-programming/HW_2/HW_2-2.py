3bb08cfd2d24df48aa6d692a7cbb4c1fd27a61f4
Lev Vasilyev <wanbergg@gmail.com>
2022-10-25 20:26:28 +0300

    Second task

---
+++ GB-dev-prog_Introduction-to-programming/HW_2/HW_2-2.py
@@ -0,0 +1,20 @@
+#Как бы я реально сделал
+#arr=[1,2,3,4,5,6]
+#revArr=[]
+#for i in range(len(arr),-1,-1):
+#    revArr.append(i)
+#print(revArr)
+
+#Как, наверное, ожидается
+arr=[1,2,3,4,5,6]
+arrLen=len(arr)
+i=0
+while i<arrLen-1:
+    j=0
+    while j<arrLen-i-1:
+        temp=arr[j]
+        arr[j]=arr[j+1]
+        arr[j+1]=temp
+        j+=1
+    i+=1
+print(arr)

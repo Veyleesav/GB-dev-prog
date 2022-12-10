3e0eba03d86acdd9074221c9914bfe66b6aaf272
Veyleesav <wanbergg@gmail.com>
2022-12-10 21:07:49 +0300

    HW_6

---
+++ GB-dev-prog_Introduction-to-cs/HW_6/Task_41/HW_6-Task_41.cs
@@ -0,0 +1,21 @@
+﻿/*
+Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+0, 7, 8, -2, -2 -> 2
+
+-1, -7, 567, 89, 223-> 3
+*/
+
+Console.WriteLine("Enter numbers:");
+string[] arr = Console.ReadLine().Split(new string[] { ","," " ," ,"," , ","; ",";","; "," ; "}, StringSplitOptions.RemoveEmptyEntries);
+
+int counter = 0;
+foreach (string i in arr)
+{
+    if (Convert.ToInt32(i)>0)
+    {
+        counter++;
+    }
+}
+
+Console.WriteLine($"{counter}");
---
+++ GB-dev-prog_Introduction-to-cs/HW_6/Task_43/HW_6-Task_43.cs
@@ -0,0 +1,26 @@
+﻿/*
+Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+*/
+
+/*
+k1 * x + b1 = k2 * x + b2
+(k1 - k2) * x = b2 - b1
+x = (b2 - b1) / (k1 - k2)
+y = k1 * x + b1
+*/
+
+Console.WriteLine("Enter b1");
+decimal b1 = Convert.ToDecimal(Console.ReadLine());
+Console.WriteLine("Enter k1");
+decimal k1 = Convert.ToDecimal(Console.ReadLine());
+Console.WriteLine("Enter b2");
+decimal b2 = Convert.ToDecimal(Console.ReadLine());
+Console.WriteLine("Enter k2:");
+decimal k2 = Convert.ToDecimal(Console.ReadLine());
+
+decimal x = (b2 - b1) / (k1 - k2);
+decimal y = k1 * x + b1;
+
+Console.WriteLine($"({x};{y})");

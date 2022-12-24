b01ba32800ba7fd7fd6acfba0680fbd2e7be54dc
Veyleesav <wanbergg@gmail.com>
2022-12-24 14:55:53 +0300

    HW 9
    Recursion

---
+++ GB-dev-prog_Introduction-to-cs/HW_9/Task_66/HW_9-Task_66.cs
@@ -0,0 +1,29 @@
+﻿/* 
+Задача 66: Задайте значения M и N.
+Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30
+*/
+
+Console.WriteLine("Enter M:");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter N:");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine();
+
+int sum = m;
+int GetNum(int m, int n)
+{
+    if (m < n)
+    {
+        sum += (m+1);
+        m++;
+        GetNum(m,n);
+    }
+    return sum;
+}
+
+Console.WriteLine($"{GetNum(m,n)}");
---
+++ GB-dev-prog_Introduction-to-cs/HW_9/Task_68/HW_9-Task_68.cs
@@ -0,0 +1,33 @@
+﻿/*
+Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии.
+Даны два неотрицательных числа m и n.
+m = 2, n = 3 -> A(m,n) = 9
+m = 3, n = 2 -> A(m,n) = 29
+*/
+Console.WriteLine("Enter M:");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter N:");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine();
+
+int Ackermann(int m, int n) 
+/*С учетом ограничений учебных задач используем int,
+//т.к если мы выйдем за ТЗ и применим, например Ackermann(4,4),
+то количество цифр в порядке этого числа многократно превзойдет
+количество атомов в наблюдаемой части Вселенной. А это много.
+*/
+{
+    if(m == 0)
+    {
+        return n + 1;
+    }
+    if(m > 0 && n == 0)
+    {
+        return Ackermann(m - 1, 1);
+    }
+    return Ackermann(m - 1, Ackermann(m, n - 1));
+}
+
+Console.WriteLine($"{Ackermann(m,n)}");
